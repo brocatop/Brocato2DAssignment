f41637871b860561f0e99e7792fe24f231970471
Patrick Brocato <patrickbrocato@gmail.com>
2019-03-31 22:23:23 -0400

    Initial commit

---
+++ Brocato2DAssignment/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Brocato2DAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawingPanel = new System.Windows.Forms.Panel();
+            this.drawPolygonButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // drawingPanel
+            // 
+            this.drawingPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.drawingPanel.Location = new System.Drawing.Point(30, 31);
+            this.drawingPanel.Name = "drawingPanel";
+            this.drawingPanel.Size = new System.Drawing.Size(536, 600);
+            this.drawingPanel.TabIndex = 0;
+            this.drawingPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.drawingPanel_Paint);
+            this.drawingPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawingPanel_MouseDown);
+            // 
+            // drawPolygonButton
+            // 
+            this.drawPolygonButton.Location = new System.Drawing.Point(870, 545);
+            this.drawPolygonButton.Name = "drawPolygonButton";
+            this.drawPolygonButton.Size = new System.Drawing.Size(233, 44);
+            this.drawPolygonButton.TabIndex = 2;
+            this.drawPolygonButton.Text = "Draw Polygon/Start Over";
+            this.drawPolygonButton.UseVisualStyleBackColor = true;
+            this.drawPolygonButton.Click += new System.EventHandler(this.drawPolygonButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(581, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(581, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(581, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(581, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(600, 545);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(233, 28);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(596, 511);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Brush:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(581, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 659);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawPolygonButton);
+            this.Controls.Add(this.drawingPanel);
+            this.MaximumSize = new System.Drawing.Size(1152, 715);
+            this.MinimumSize = new System.Drawing.Size(1152, 715);
+            this.Name = "Form1";
+            this.Text = "Brocato 2D Paint Assignment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel drawingPanel;
+        private System.Windows.Forms.Button drawPolygonButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Brocato2DAssignment/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Brocato2DAssignment
+{
+    public partial class Form1 : Form
+    {
+        //Initialization variables
+        //pointList to hold the points for drawing, the pen is created for users seeing where they are drawing, and the ready variables are meant to toggle if the
+        //drawing panel is ready to be cleared or if it is ready to be drawn on
+        List<Point> pointList = new List<Point>();
+        Pen pen = new Pen(Color.Black, 2);
+        bool readyToDraw = false;
+        bool readyToClear = false;
+
+
+        //Form Initialization
+        //Sets the proper settings for how the UI needs to look, including instructions on the UI for how to use the application
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            LoadComboBox();
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.SelectedIndex = 0;
+            label1.Text = "Step 1: Click inside the drawing panel to create a point";
+            label2.Text = "Step 2: After you make your polygon choose your brush";
+            label3.Text = "Step 3: Choose the color(s) of the polygon";
+            label4.Text = "Step 4: Click the Start Over Button to Clear the canvas";
+            label6.Text = "Step 5: Repeat and enjoy the program!  -PB";
+        }
+
+        //Method for drawing the drawing panel, allows the user to choose their type of brush and color before painting onto the drawing panel
+        private void drawingPanel_Paint(object sender, PaintEventArgs e)
+        {
+            //Graphics object that will be used throughout the method
+            Graphics g = e.Graphics;
+            //If the user has clicked on the canvas, creating a point, then the following code allows the user to choose their polygon's colors and brush
+            if (pointList.Count > 0)
+            {
+                //If the polygon is ready to be drawn
+                if (readyToDraw == true)
+                {
+                    //Switch statement that takes in what the user decides is their brush from the combobox on the UI. By default, the program selects a solid brush
+                    switch (comboBox1.SelectedIndex)
+                    {
+                        //Chooses the color for the solid brush
+                        case 0:
+                            MessageBox.Show("Please choose the color of your polygon");
+
+                            ColorDialog solidColorDialog = new ColorDialog();
+
+                            solidColorDialog.ShowDialog();
+
+                            Brush solidBrush = new SolidBrush(solidColorDialog.Color);
+
+                            g.FillPolygon(solidBrush, pointList.ToArray());
+                            pointList.Clear();
+                            pointList = new List<Point>();
+                            readyToDraw = false;
+                            readyToClear = true;
+                            break;
+
+
+                        //Chooses the two colors for the gradient effect of the polygon
+                        case 1:
+                            MessageBox.Show("Please choose the first color for your gradient polygon");
+
+                            ColorDialog gradientDialog1 = new ColorDialog();
+
+                            gradientDialog1.ShowDialog();
+
+                            Color gradientColor1 = gradientDialog1.Color;
+
+                            MessageBox.Show("Please choose the second color for your gradient polygon");
+
+                            ColorDialog gradientDialog2 = new ColorDialog();
+
+                            gradientDialog2.ShowDialog();
+
+                            Color gradientColor2 = gradientDialog2.Color;
+
+                            LinearGradientBrush brush = new LinearGradientBrush(pointList.ToArray()[0], pointList.ToArray()[pointList.ToArray().Length - 1], gradientColor1, gradientColor2);
+
+                            g.FillPolygon(brush, pointList.ToArray());
+                            pointList.Clear();
+                            pointList = new List<Point>();
+                            readyToDraw = false;
+                            readyToClear = true;
+                            break;
+
+                        //Chooses the two colors for the horizontal hatch pattern of the polygon
+                        case 2:
+                            MessageBox.Show("Please choose the first color for your horizontal hatch polygon");
+
+                            ColorDialog hatchDialog1 = new ColorDialog();
+
+                            hatchDialog1.ShowDialog();
+
+                            Color hatchColor1 = hatchDialog1.Color;
+
+                            MessageBox.Show("Please choose the second color for your horizontal hatch polygon");
+
+                            ColorDialog hatchDialog2 = new ColorDialog();
+
+                            hatchDialog2.ShowDialog();
+
+                            Color hatchColor2 = hatchDialog2.Color;
+
+                            HatchBrush hatchBrush = new HatchBrush(HatchStyle.Horizontal, hatchColor1, hatchColor2);
+                            g.FillPolygon(hatchBrush, pointList.ToArray());
+                            pointList.Clear();
+                            pointList = new List<Point>();
+                            readyToDraw = false;
+                            readyToClear = true;
+                            break;
+
+
+                        //Chooses the two colors for the forward diagonal hatch pattern of the polygon
+                        case 3:
+                            MessageBox.Show("Please choose the first color for your diagonal hatch polygon");
+
+                            ColorDialog diagDialog1 = new ColorDialog();
+
+                            diagDialog1.ShowDialog();
+
+                            Color diagColor1 = diagDialog1.Color;
+
+                            MessageBox.Show("Please choose the second color for your diagonal hatch polygon");
+                            ColorDialog diagDialog2 = new ColorDialog();
+
+                            diagDialog2.ShowDialog();
+
+                            Color diagColor2 = diagDialog2.Color;
+
+                            HatchBrush diagBrush = new HatchBrush(HatchStyle.ForwardDiagonal, diagColor1, diagColor2);
+                            g.FillPolygon(diagBrush, pointList.ToArray());
+                            pointList.Clear();
+                            pointList = new List<Point>();
+                            readyToDraw = false;
+                            readyToClear = true;
+                            break;
+
+
+                        //Chooses the two colors for the cross hatch pattern of the polygon
+                        case 4:
+                            MessageBox.Show("Please choose the first color for your cross hatch polygon");
+
+                            ColorDialog crossDialog1 = new ColorDialog();
+
+                            crossDialog1.ShowDialog();
+
+                            Color crossColor1 = crossDialog1.Color;
+
+                            MessageBox.Show("Please choose the second color for your cross hatch polygon");
+
+                            ColorDialog crossDialog2 = new ColorDialog();
+
+                            crossDialog2.ShowDialog();
+
+                            Color crossColor2 = crossDialog2.Color;
+
+                            HatchBrush crossBrush = new HatchBrush(HatchStyle.Cross, crossColor1, crossColor2);
+                            g.FillPolygon(crossBrush, pointList.ToArray());
+                            pointList.Clear();
+                            pointList = new List<Point>();
+                            readyToDraw = false;
+                            readyToClear = true;
+                            break;
+                    }
+
+                }
+                //If the polygon is not ready to be drawn
+                else if (readyToDraw == false)
+                {
+                    foreach(Point p in pointList)
+                    {
+                        Rectangle r = new Rectangle(p.X, p.Y, 2, 2);
+                        e.Graphics.DrawEllipse(pen, r);
+                    }
+                }
+            }          
+        }
+
+        //When the user clicks on the drawing panel, add the point to the list of points that the polygon will be created from
+        private void drawingPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point p = new Point(e.X, e.Y);
+            pointList.Add(p);
+            drawingPanel.Refresh();
+        }
+
+        //When the user is satisfied with their polygon, then panel refreshes with their polygon
+        private void drawPolygonButton_Click(object sender, EventArgs e)
+        {
+            if(readyToClear == true)
+            {
+                readyToClear = false;
+                drawingPanel.Refresh();
+            }
+            else
+            {
+                readyToDraw = true;
+                drawingPanel.Refresh();
+            }
+        }
+
+        //Loads the combobox with the choices of brushes
+        private void LoadComboBox()
+        {
+            comboBox1.Items.Add("Solid Brush");
+            comboBox1.Items.Add("Gradient Brush");
+            comboBox1.Items.Add("Horizontel Hatch Brush");
+            comboBox1.Items.Add("Horizontel Diagonal Brush");
+            comboBox1.Items.Add("Horizontel Cross Brush");
+        }
+    }
+}
